c7a03f5e4bde371d36cc74ed16d48b9b7a316b95
GerkJa <gerkmanj@arcada.fi>
2024-04-11 15:31:21 +0000

    test

--- hotel-api/app.py
+++ hotel-api/app.py
@@ -1,35 +1,102 @@
+import os, psycopg
+from psycopg.rows import dict_row
 from flask import Flask, request
-from flask_cors import CORS 
+from flask_cors import CORS
+from dotenv import load_dotenv 
 
-PORT=8091
+# pip install psycopg_binary python-dotenv
+
+load_dotenv()
+
+PORT=8091 # EGEN PORT!
+
+db_url = os.environ.get("DB_URL")
+print(os.environ.get("FOO"))
+
+conn = psycopg.connect(db_url, autocommit=True, row_factory=dict_row)
 
 app = Flask(__name__)
 CORS(app) # Tillåt cross-origin requests
 
-#@app.route("/")
-#def hello():
-#    return f"<h1 style='color: blue; font-size: 500%;'>Hello, {request.remote_addr}!</h1>"
+roomsTEMP = [
+    { 'number': 101, 'type': "single" },
+    { 'number': 202, 'type': "double" },
+    { 'number': 303, 'type': "suite" }
+]
 
-@app.route("/")
-def greeting():
-    return{'greeting':"Hello, from flask!",'ip':request.remote_addr}    
+@app.route("/", )
+def info():
+    return "Hotel API, endpoints /rooms, /bookings"
 
-@app.route("/test", methods=[ "GET", "POST"])
-def test():
-    if request.method == "POST":
-        new_id = 555
-        return {
-            'msg': f"här får du id: {id}",
-            'method': request.method
+
+@app.route("/guests", methods=['GET'])
+def guests_endpoint():
+    with conn.cursor() as cur:
+            cur.execute("""SELECT * 
+                        FROM hotel_guest 
+                        ORDER BY firstname""")
+            return cur.fetchall()
+
+
+@app.route("/rooms", methods=['GET', 'POST'])
+def rooms_endpoint():
+    if request.method == 'POST':
+        request_body = request.get_json()
+        print(request_body)
+        roomsTEMP.append(request_body)
+        return { 
+            'msg': f"Du har skapat ett nytt rum, id: {len(roomsTEMP)-1}!"
         }
-    if request.method == 'DELETE':
-        return {
-            'msg': f"Du har raderat"
-        }
+    else:
+        with conn.cursor() as cur:
+            cur.execute("""SELECT * 
+                        FROM hotel_room 
+                        ORDER BY room_number""")
+            return cur.fetchall()
+
+@app.route("/rooms/<int:id>", methods=['GET', 'PUT', 'PATCH', 'DELETE'] )
+def one_room_endpoint(id):
+        if request.method == 'GET':
+            with conn.cursor() as cur:
+                cur.execute("""
+                    SELECT * 
+                    FROM hotel_room 
+                    WHERE id = %s""", [id])
+
+                return cur.fetchone()
+        
+@app.route("/bookings", methods=['GET', 'POST'])
+def bookings():
+    if request.method == 'GET':
+        with conn.cursor() as cur:
+            cur.execute("""SELECT * 
+                        FROM hotel_booking 
+                        ORDER BY datefrom""")
+            return cur.fetchall()
+        
+    if request.method == 'POST':
+        body = request.get_json()
+        with conn.cursor() as cur:
+            cur.execute("""
+                INSERT INTO hotel_booking (
+                    room_id, 
+                    guest_id,
+                    datefrom
+                ) VALUES (
+                    %s, 
+                    %s, 
+                    %s
+                ) RETURNING id""", [ 
+                body['room'], 
+                body['guest'], 
+                body['datefrom'] 
+            ])
+            result = cur.fetchone()
     
+        return { "msg": "Du har bokat ett rum!", "result": result }
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=True, ssl_context=(
         '/etc/letsencrypt/fullchain.pem', 
         '/etc/letsencrypt/privkey.pem'
     ))
-
